437d5c51439eb991d22cbe48291d06f58609c3b1
Denise Barrera <denise.barrera@outlook.com.ar>
2025-10-10 01:40:32 -0300

    Agrego frontend Denu

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,15 +1,15 @@
 function AdminPanel() {
-  return (
-    <div className="admin-panel">
-      <aside className="sidebar">
-        <h3>Admin Menu</h3>
-      </aside>
-      <main className="main-content">
-        <button>Ventas Totales</button>
-        <button>Clientes Activos</button>
-      </main>
-    </div>
-  );
+    return (
+        <div className="admin-panel">
+            <aside className="sidebar">
+                <h3>Admin Menu</h3>
+            </aside>
+            <main className="main-content">
+                <button>Ventas Totales</button>
+                <button>Clientes Activos</button>
+            </main>
+        </div>
+    );
 }
 
 export default AdminPanel;
